3615f90f140118cb169142a63ec8dcae46831269
David He <jialhe85@gmail.com>
2017-12-06 15:32:38 +1100

    Card wip

---
+++ src/components/Home/BestSellers/Card.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Image, Grid, Card, Icon } from 'semantic-ui-react';
+
+const Card = ({name, price, descr, image}) => (
+  <Card>
+    <Image src={image} />
+    <Card.Content>
+      <Card.Header>{name}</Card.Header>
+      <Card.Meta>
+        <span className="date">{price}</span>
+      </Card.Meta>
+      <Card.Description>
+        {descr}
+      </Card.Description>
+    </Card.Content>
+  </Card>
+);
+
+export { Card };
+
+    // <Card.Content extra>
+    //   <a>
+    //     <Icon name="user" />
+    //     22 Friends
+    //   </a>
+    // </Card.Content>
--- src/components/Home/BestSellers/index.js
+++ src/components/Home/BestSellers/index.js
@@ -3,44 +3,24 @@ import React from 'react';
 import { Image, Grid, Card, Icon } from 'semantic-ui-react';
 
 import elliot from 'images/elliot.jpg';
+import { Card } from './Card';
 
 const BestSellers = () => {
   return (
     <Grid centered>
       <Grid.Row centered columns={4}>
         <Grid.Column>
-          <CardExampleCard />
+          <Card />
         </Grid.Column>
         <Grid.Column>
-          <CardExampleCard />
+          <Card />
         </Grid.Column>
         <Grid.Column>
-          <CardExampleCard />
+          <Card />
         </Grid.Column>
       </Grid.Row>
     </Grid>
   );
 };
-
-const CardExampleCard = () => (
-  <Card>
-    <Image src={elliot} />
-    <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className="date">Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name="user" />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
-);
 
 export { BestSellers };
